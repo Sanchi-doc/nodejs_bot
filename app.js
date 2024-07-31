1df0866a72c303c3fa22f0ff9480c342f2559aaf
Alex Babichenko <babichenkoaleksandr@gmail.com>
2024-07-31 12:27:09 +0300

    add health

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
-const {bot} = require('./utils/bot');
+const healthRoutes = require('./routes/health'); 
+const { bot } = require('./utils/bot');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,15 +12,16 @@ app.use(express.json());
 // Use routes
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
+app.use('/health', healthRoutes); 
 
 bot.launch()
-.then( () => {
+  .then(() => {
     console.log('Bot started');
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log('Error starting bot:', error);
-})
+  });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
-
