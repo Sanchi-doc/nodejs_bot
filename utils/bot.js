b0a127d9915607faec7a417bc31ae4cd2bfb8e9f
Alex Babichenko <babichenkoaleksandr@gmail.com>
2024-07-31 13:44:31 +0300

    update web app

--- utils/bot.js
+++ utils/bot.js
@@ -4,7 +4,6 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start((ctx) => {
-  const userId = ctx.from.id; // Get the unique Telegram user ID
   const webAppUrl = process.env.WEB_APP_URL;
 
   if (!webAppUrl) {
@@ -13,17 +12,23 @@ bot.start((ctx) => {
     return;
   }
 
-  const loginUrl = `${webAppUrl}`; // Generate the login URL with userId parameter
-
   const inlineKeyboard = {
     inline_keyboard: [[
-      { text: 'Log in to the website', url: loginUrl }
+      { text: 'Log in to the website', web_app: { url: webAppUrl } }
     ]]
   };
 
   ctx.reply('Welcome! Please log in to the website using the button below:', { reply_markup: inlineKeyboard });
 });
 
+// Обработка данных, отправленных из веб-приложения
+bot.on('message', (ctx) => {
+  if (ctx.message && ctx.message.web_app_data) {
+    const data = JSON.parse(ctx.message.web_app_data.data);
+    console.log('Data received from web app:', data);
+    ctx.reply(`Data received: ${JSON.stringify(data)}`);
+  }
+});
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
