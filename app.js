c29b468c8fa3b504ae0e18604dcac2b1a03c3d3a
i.perekhodchenko <i.perekhodchenko@favbet.tech>
2024-08-04 19:21:13 +0300

    add login signin routes

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const { bot } = require('./utils/bot');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
+const helmet = require("helmet");
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(helmet());
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -17,29 +19,7 @@ app.use(bodyParser.json())
 // Use routes
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
-app.use('/health', healthRoutes); 
-
-
-app.post('/api/auth/login', (req, res) => {
-  const { email, password } = req.body
-  // Validate credentials (replace with your own logic)
-  if (email === 'user@example.com' && password === 'password') {
-    const token = jwt.sign({ email }, 'your-secret-key', { expiresIn: '1h' })
-    res.json({ token, user: { email } })
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' })
-  }
-})
-
-app.get('/api/auth/user', (req, res) => {
-  const token = req.headers.authorization.split(' ')[1]
-  try {
-    const decoded = jwt.verify(token, 'your-secret-key')
-    res.json({ user: { email: decoded.email } })
-  } catch (error) {
-    res.status(401).json({ message: 'Invalid token' })
-  }
-})
+app.use('/health', healthRoutes);
 
 bot.launch()
   .then(() => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { findUserByTelegramId, createUserWithTelegramId } = require('../utils/memoryDb');
 
 router.get('/login', (req, res) => {
-  const userId = req.query.userId;
-
-  if (!userId) {
-    return res.status(400).send('userId is required');
-  }
-
-  const user = findUserByTelegramId(userId);
-
-  if (user) {
-    res.send(`Welcome, ${user.name}`);
+  const { email, password } = req.body
+  // Validate credentials (replace with your own logic)
+  if (email === 'user@example.com' && password === 'password') {
+    const token = jwt.sign({ email }, 'your-secret-key', { expiresIn: '1h' })
+    res.json({ token, user: { email } })
   } else {
-    const newUser = createUserWithTelegramId(userId);
-    res.send(`Registration successful! Welcome, ${newUser.name}`);
+    res.status(401).json({ message: 'Invalid credentials' })
   }
 });
+
+router.post('/register', async (req, res) => {
+  const {username, password} = req.body;
+
+  console.log('------------', username, password);
+  // // Перевірка, чи користувач з таким ім'ям вже існує
+  // if (users.some(user => user.username === username)) {
+  //   return res.status(400).json({message: 'User already exists'});
+  // }
+  //
+  // // Хешування паролю перед збереженням в базі даних
+  // const hashedPassword = await bcrypt.hash(password, 10); // 10 - сіль для хешування
+  //
+  // // Збереження нового користувача
+  // const newUser = {username, password: hashedPassword};
+  // users.push(newUser);
+
+  res.json({message: 'User registered successfully'});
+});
+
+router.get('/user', (req, res) => {
+  const token = req.headers?.authorization?.split(' ')[1]
+  try {
+    const decoded = jwt.verify(token, 'your-secret-key')
+    res.json({ user: { email: decoded.email } })
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' })
+  }
+})
 
 module.exports = router;
