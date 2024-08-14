51946631e2ebff7c011c4f4d490e4fa6173992d9
i.perekhodchenko <i.perekhodchenko@favbet.tech>
2024-08-14 22:56:36 +0300

    add bot log

--- utils/bot.js
+++ utils/bot.js
@@ -7,10 +7,19 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 bot.start((ctx) => {
   const user = ctx.message.from;
 
-  // Output token to console
-  console.log(`Generated token for user ${JSON.stringify(user)}`);
+  const payload = {
+    id: user.id,
+    first_name: user.first_name,
+    username: user.username,
+    language_code: user.language_code
+  };
 
-  const webAppUrl = `${process.env.WEB_APP_URL}?token=`;
+  const token = jwt.sign(payload, process.env.SESSION_SECRET, { expiresIn: '1h' });
+
+  // Output token to console
+  console.log(`Generated token for user ${user.username}: ${token}`);
+
+  const webAppUrl = `${process.env.WEB_APP_URL}?token=${token}`;
 
   const inlineKeyboard = {
     inline_keyboard: [[
@@ -22,8 +31,8 @@ bot.start((ctx) => {
   ctx.reply('Welcome! Please log in to the website using the button below:', { reply_markup: inlineKeyboard });
 });
 
-bot.launch()
-  .then(() => console.log('Bot started'))
-  .catch(error => console.error('Error starting bot:', error));
+// bot.launch()
+//   .then(() => console.log('Bot started'))
+//   .catch(error => console.error('Error starting bot:', error));
 
 module.exports = { bot };
