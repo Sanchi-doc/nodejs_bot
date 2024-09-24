26ad28cf7810fb9eec251788e53f717060a05077
i.perekhodchenko <i.perekhodchenko@favbet.tech>
2024-09-24 17:34:47 +0300

    add  log 10

--- utils/bot.js
+++ utils/bot.js
@@ -4,54 +4,54 @@ const jwt = require('jsonwebtoken');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-// bot.start((ctx) => {
-//   const user = ctx.message.from;
-//
-//   console.log('Received user:', user);
-//
-//   // Генерация URL с токеном и дополнительными параметрами
-//   const webAppUrl = `${process.env.WEB_APP_URL}tg?tgId=${user.id}&username=${encodeURIComponent(user.username)}`;
-//
-//   console.log(`Generated URL for user: ${webAppUrl}`);
-//
-//   const inlineKeyboard = {
-//     inline_keyboard: [[
-//       { text: 'Open Web App', web_app: { url: webAppUrl } }
-//     ]]
-//   };
-//
-//   // Отправка кнопки пользователю
-//   ctx.reply('Welcome! Please log in to the website using the button below:', { reply_markup: inlineKeyboard });
-// });
+bot.start((ctx) => {
+  const user = ctx.message.from;
 
-bot.start(async (ctx) => {
-  try {
-     console.log('Received ctx:', ctx.message);
-    // Step 1: Send the image
-    ctx.replyWithPhoto(
-      { url: 'https://emp.betinvest.xyz/staticcdn/static/img/tournaments/870x400.png' },  // Replace with your actual image URL or file_id
-      { caption: 'Please share your phone number by pressing the button below.' }
-    );
+  console.log('Received user:', user);
 
-    //Step 2: Send the contact request keyboard
-    ctx.reply('Share your phone number:', {
-      reply_markup: {
-        keyboard: [
-          [
-            {
-              text: 'Share phone number',
-              request_contact: true, // This will request the user's phone number
-            }
-          ]
-        ],
-        resize_keyboard: true, // Adjust the keyboard size to fit the button
-        one_time_keyboard: true, // Hide the keyboard after the user presses the button
-      },
-    });
-  } catch (error) {
-    console.error('Error sending image or requesting contact:', error);
-  }
+  // Генерация URL с токеном и дополнительными параметрами
+  const webAppUrl = `${process.env.WEB_APP_URL}tg?tgId=${user.id}&username=${encodeURIComponent(user.username)}`;
+
+  console.log(`Generated URL for user: ${webAppUrl}`);
+
+  const inlineKeyboard = {
+    inline_keyboard: [[
+      { text: 'Open Web App', web_app: { url: webAppUrl } }
+    ]]
+  };
+
+  // Отправка кнопки пользователю
+  ctx.reply('Welcome! Please log in to the website using the button below:', { reply_markup: inlineKeyboard });
 });
+
+// bot.start(async (ctx) => {
+//   try {
+//      console.log('Received ctx:', ctx.message);
+//     // Step 1: Send the image
+//     ctx.replyWithPhoto(
+//       { url: 'https://emp.betinvest.xyz/staticcdn/static/img/tournaments/870x400.png' },  // Replace with your actual image URL or file_id
+//       { caption: 'Please share your phone number by pressing the button below.' }
+//     );
+//
+//     //Step 2: Send the contact request keyboard
+//     ctx.reply('Share your phone number:', {
+//       reply_markup: {
+//         keyboard: [
+//           [
+//             {
+//               text: 'Share phone number',
+//               request_contact: true, // This will request the user's phone number
+//             }
+//           ]
+//         ],
+//         resize_keyboard: true, // Adjust the keyboard size to fit the button
+//         one_time_keyboard: true, // Hide the keyboard after the user presses the button
+//       },
+//     });
+//   } catch (error) {
+//     console.error('Error sending image or requesting contact:', error);
+//   }
+// });
 
 bot.on('contact', (ctx) => {
   console.log("bot on contact", ctx.message)
