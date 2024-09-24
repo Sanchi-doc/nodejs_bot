60a3dac832a615e2b5d9db5dcf4ceff0fbcd5ce2
i.perekhodchenko <i.perekhodchenko@favbet.tech>
2024-09-24 15:52:52 +0300

    add  log 4

--- utils/bot.js
+++ utils/bot.js
@@ -4,24 +4,54 @@ const jwt = require('jsonwebtoken');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
+// bot.start((ctx) => {
+//   const user = ctx.message.from;
+//
+//   console.log('Received user:', user);
+//
+//   // Генерация URL с токеном и дополнительными параметрами
+//   const webAppUrl = `${process.env.WEB_APP_URL}tg?tgId=${user.id}&username=${encodeURIComponent(user.username)}`;
+//
+//   console.log(`Generated URL for user: ${webAppUrl}`);
+//
+//   const inlineKeyboard = {
+//     inline_keyboard: [[
+//       { text: 'Open Web App', web_app: { url: webAppUrl } }
+//     ]]
+//   };
+//
+//   // Отправка кнопки пользователю
+//   ctx.reply('Welcome! Please log in to the website using the button below:', { reply_markup: inlineKeyboard });
+// });
+
 bot.start((ctx) => {
-  const user = ctx.message.from;
+  // Step 1: Send an image
+  ctx.replyWithPhoto({ url: 'https://emp.betinvest.xyz/staticcdn/static/img/tournaments/870x400.png' }, {
+    caption: 'Please share your phone number'
+  })
+    .then(() => {
+      // Step 2: Send a keyboard with a button to request contact
+      ctx.reply('Share your phone number by pressing the button below.', {
+        reply_markup: {
+          keyboard: [
+            [
+              {
+                text: 'Share phone number',
+                request_contact: true, // This will request the user's phone number
+              }
+            ]
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      });
+    });
+});
 
-  console.log('Received user:', user);
-  
-  // Генерация URL с токеном и дополнительными параметрами
-  const webAppUrl = `${process.env.WEB_APP_URL}tg?tgId=${user.id}&username=${encodeURIComponent(user.username)}`;
-
-  console.log(`Generated URL for user: ${webAppUrl}`);
-
-  const inlineKeyboard = {
-    inline_keyboard: [[
-      { text: 'Open Web App', web_app: { url: webAppUrl } }
-    ]]
-  };
-
-  // Отправка кнопки пользователю
-  ctx.reply('Welcome! Please log in to the website using the button below:', { reply_markup: inlineKeyboard });
+bot.on('contact', (ctx) => {
+  console.log("bot on contact", ctx.message)
+  const phoneNumber = ctx.message.contact.phone_number;
+  ctx.reply(`Thank you! Your phone number is: ${phoneNumber}`);
 });
 
 // bot.launch()
